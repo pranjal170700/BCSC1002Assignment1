20c6d5a14184f9ca50f81ee1bd4dd29520793576
Pranjal Singh <singhpranjal6973@gmail.com>
2020-08-28 22:11:24 +0530

    add private fields for Student.java class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,10 @@
 package definitions;
 
 public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumber;
+    private int numberOfBooksIssued;
+    private Book[] namesOfBooksIssued;
 }
